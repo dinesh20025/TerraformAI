ea65888ab76128b2c089664f1d76db93dd74d62e
Dinesh Suthar <dinesh.suthar275@gmail.com>
2025-09-09 02:23:28 +0200

    Update variables.tf

--- rg-prod-esg2-we-001/modules/acl/variables.tf
+++ rg-prod-esg2-we-001/modules/acl/variables.tf
@@ -1,1 +1,25 @@
-
+variable "aad_group_object_id" {
+  description = "Group object ID"
+  type        = string
+  default     = "Standard"
+}
+variable "filesystem_name" {
+  description = "Filesystem name"
+  type        = string
+  default     = "Standard"
+}
+variable "storage_account_id" {
+  description = "Storage account id"
+  type        = string
+  default     = "Standard"
+}
+variable "bu_name" {
+  description = "Storage account id"
+  type        = string
+  default     = "Standard"
+}
+variable "ops_group_object_id" {
+  description = "Group object ID"
+  type        = string
+  default     = "Standard"
+}
