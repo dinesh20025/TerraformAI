8609f4ce94af6236505d17e5105eece5b3777843
Dinesh Suthar <dinesh.suthar275@gmail.com>
2025-09-09 02:26:47 +0200

    Create outputs.tf

---
+++ rg-prod-esg2-we-001/modules/storage-account/outputs.tf
@@ -0,0 +1,9 @@
+output "sa_name" {
+  value = azurerm_storage_account.storage_account.name
+}
+output "sa_id" {
+  value = azurerm_storage_account.storage_account.id
+}
+output "tfsa_name" {
+  value = azurerm_storage_account.storage_account.name
+}
