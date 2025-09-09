7356f534426450f55959b32694cc1da600393b48
Dinesh Suthar <dinesh.suthar275@gmail.com>
2025-09-09 02:30:23 +0200

    Update variables.tf

--- rg-prod-esg2-we-001/modules/sandbox-green-record/variables.tf
+++ rg-prod-esg2-we-001/modules/sandbox-green-record/variables.tf
@@ -1,1 +1,20 @@
-
+variable "ops_group_object_id" {
+  description = "Group object ID"
+  type        = string
+  default     = "Standard"
+}
+variable "filesystem_name" {
+  description = "Filesystem name"
+  type        = string
+  default     = "Standard"
+}
+variable "storage_account_id" {
+  description = "tTorage account id"
+  type        = string
+  default     = "Standard"
+}
+variable "sandbox-green-record_path" {
+  description = "sandbox-green-record_path"
+  type        = string
+  default     = "Standard"
+}
